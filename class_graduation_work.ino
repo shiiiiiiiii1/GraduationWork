d65648c90d5e464b4f73799a5f5c5ee812511f51
shoichi <shou1_822@yahoo.co.jp>
2016-10-29 21:44:43 +0900

    analog sensing

--- class_graduation_work.ino
+++ class_graduation_work.ino
@@ -31,15 +31,18 @@ void setup() {
 
   bme280.setMode(temp_press_hum_i2c_addr, osrs_t, osrs_p, osrs_h, bme280mode, t_sb, filter, spi3w_en);
   bme280.readTrim();
+
+  accelgyro.initialize();
 }
 
 void loop() {
   double temp_act, press_act, hum_act;   // 最終的に表示される値を入れる変数
-
   bme280.readData(&temp_act, &press_act, &hum_act);   // 温湿度気圧センサの値を読み込む
   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);   // 6軸加速度センサの値を読み込む
   sensors_event_t lux;
   tsl.getEvent(&lux);   // 照度センサの値を読み込む
+  float ans = analogRead(0);   // 赤外線センサ
+  float microphone = analogRead(1);   // マイク
 
   setTime(12, 38, 0, 25, 10, 2016);   // 現在の時刻を入れて実験開始 ---------------------------------------------------------------------------------
   Serial.print(year()); Serial.print("/"); Serial.print(month()); Serial.print("/"); Serial.print(day()); Serial.print(" ");
@@ -47,15 +50,17 @@ void loop() {
   Serial.print("  TEMP : "); Serial.print(temp_act);
   Serial.print("DegC / PRESS : "); Serial.print(press_act);
   Serial.print("hPa / HUM : "); Serial.print(hum_act);
-  Serial.print("% / ax : "); Serial.print(ax);
-  Serial.print("g / ay : "); Serial.print(ay);
-  Serial.print("g / az : "); Serial.print(az);
+  Serial.print("% / ACCE_X : "); Serial.print(ax);
+  Serial.print("g / ACCE_Y : "); Serial.print(ay);
+  Serial.print("g / ACCE_Z : "); Serial.print(az);
   Serial.print("g / LUX : ");
   if (lux.light) {
     Serial.print(lux.light); Serial.print("lux");
   } else {
     Serial.print("'Sensor overload'");
   }
+  Serial.print(" / ans : "); Serial.print(ans);
+  Serial.print(" / microphone : "); Serial.print(microphone); Serial.println("");
 
   delay(1000);
 }
