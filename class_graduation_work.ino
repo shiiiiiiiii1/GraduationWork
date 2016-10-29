186c7125d1fde120a8a51b94debcdec751ac858f
shoichi <shou1_822@yahoo.co.jp>
2016-10-29 21:17:01 +0900

    lux sensing

--- class_graduation_work.ino
+++ class_graduation_work.ino
@@ -4,6 +4,8 @@
 #include <SSCI_BME280.h>   // 温湿度気圧センサ
 #include <I2Cdev.h>   // 6軸加速度センサ
 #include <MPU6050.h>   // 6軸加速度センサ
+#include <Adafruit_Sensor.h>   // 照度センサ
+#include <Adafruit_TSL2561_U.h>   // 照度センサ
 
 
 SSCI_BME280 bme280;
@@ -12,6 +14,8 @@ uint8_t temp_press_hum_i2c_addr = 0x76;   // 温湿度気圧センサ I2C Addres
 MPU6050 accelgyro;   // 6軸加速度センサ
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
+
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);   // 照度センサ
 
 void setup() {
   uint8_t osrs_t = 1;   // Temperature oversampling x 1
@@ -24,29 +28,34 @@ void setup() {
 
   Serial.begin(9600);
   Wire.begin();
+
   bme280.setMode(temp_press_hum_i2c_addr, osrs_t, osrs_p, osrs_h, bme280mode, t_sb, filter, spi3w_en);
-
   bme280.readTrim();
-
-  accelgyro.initialize();
 }
-
 
 void loop() {
   double temp_act, press_act, hum_act;   // 最終的に表示される値を入れる変数
 
   bme280.readData(&temp_act, &press_act, &hum_act);   // 温湿度気圧センサの値を読み込む
   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);   // 6軸加速度センサの値を読み込む
+  sensors_event_t lux;
+  tsl.getEvent(&lux);   // 照度センサの値を読み込む
 
   setTime(12, 38, 0, 25, 10, 2016);   // 現在の時刻を入れて実験開始 ---------------------------------------------------------------------------------
   Serial.print(year()); Serial.print("/"); Serial.print(month()); Serial.print("/"); Serial.print(day()); Serial.print(" ");
   Serial.print(hour()); Serial.print(":"); Serial.print(minute()); Serial.print(":"); Serial.print(second());
   Serial.print("  TEMP : "); Serial.print(temp_act);
-  Serial.print(" DegC / PRESS : "); Serial.print(press_act);
-  Serial.print(" hPa / HUM : "); Serial.print(hum_act);
-  Serial.print(" % / ax : "); Serial.print(ax);
-  Serial.print(" g / ay : "); Serial.print(ay);
-  Serial.print(" g / az : "); Serial.println(az);
+  Serial.print("DegC / PRESS : "); Serial.print(press_act);
+  Serial.print("hPa / HUM : "); Serial.print(hum_act);
+  Serial.print("% / ax : "); Serial.print(ax);
+  Serial.print("g / ay : "); Serial.print(ay);
+  Serial.print("g / az : "); Serial.print(az);
+  Serial.print("g / LUX : ");
+  if (lux.light) {
+    Serial.print(lux.light); Serial.print("lux");
+  } else {
+    Serial.print("'Sensor overload'");
+  }
 
   delay(1000);
 }
