ce87f6d238b40315421ce39adc3d0b082fe59215
shoichi <shou1_822@yahoo.co.jp>
2016-11-16 01:12:54 +0900

    code rearrangement

--- class_graduation_work.ino
+++ class_graduation_work.ino
@@ -10,15 +10,20 @@
 
 SSCI_BME280 bme280;
 uint8_t temp_press_hum_i2c_addr = 0x76;   // 温湿度気圧センサ I2C Address
+double temp_act, press_act, hum_act;   // 温湿度気圧センサ変数宣言
 
 MPU6050 accelgyro;   // 6軸加速度センサ
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);   // 照度センサ
+sensors_event_t lux;
 
-int mic_pin = 3;   // 音センサピン
-int pir_pin = 13;   // 焦電センサピン
+int mic_pin = 3;   // 音センサのピン
+int microphone;
+
+int pir_pin = 13;   // 焦電センサのピン
+int ans;
 
 void setup() {
   uint8_t osrs_t = 1;   // Temperature oversampling x 1
@@ -36,35 +41,42 @@ void setup() {
   bme280.readTrim();
 
   accelgyro.initialize();
+
+// ------------------------------- 現在の時刻を入れて実験開始 -------------------------------
+  setTime(12, 38, 0, 25, 10, 2016);
 }
 
 void loop() {
-  double temp_act, press_act, hum_act;   // 最終的に表示される値を入れる変数
   bme280.readData(&temp_act, &press_act, &hum_act);   // 温湿度気圧センサの値を読み込む
   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);   // 6軸加速度センサの値を読み込む
-  sensors_event_t lux;
   tsl.getEvent(&lux);   // 照度センサの値を読み込む
-  int microphone = analogRead(mic_pin);   // マイク
-  int ans = digitalRead(pir_pin);   // 赤外線センサ
-  String pir = ans==0 ? "LOW" : "HIGH";
+  microphone = analogRead(mic_pin);   // 音センサの値を読み込む
+  microphone = map(microphone, 0, 1023, -511, 512);
+  ans = digitalRead(pir_pin);   // 赤外線センサの値を読み込む
+  // String pir = ans==0 ? "LOW" : "HIGH";
 
-  setTime(12, 38, 0, 25, 10, 2016);   // 現在の時刻を入れて実験開始 ---------------------------------------------------------------------------------
+  if(ans == 1){
+    sensing_output();
+  }
+  delay(100);
+}
+
+
+void sensing_output() {
   Serial.print(year()); Serial.print("/"); Serial.print(month()); Serial.print("/"); Serial.print(day()); Serial.print(" ");
   Serial.print(hour()); Serial.print(":"); Serial.print(minute()); Serial.print(":"); Serial.print(second());
-  Serial.print("  TEMP : "); Serial.print(temp_act);
-  Serial.print("DegC / PRESS : "); Serial.print(press_act);
-  Serial.print("hPa / HUM : "); Serial.print(hum_act);
-  Serial.print("% / ACCE_X : "); Serial.print(ax);
-  Serial.print("g / ACCE_Y : "); Serial.print(ay);
-  Serial.print("g / ACCE_Z : "); Serial.print(az);
+  Serial.print("  Temperature : "); Serial.print(temp_act);
+  Serial.print("DegC / Pressure : "); Serial.print(press_act);
+  Serial.print("hPa / Humidity : "); Serial.print(hum_act);
+  Serial.print("% / Acceleration_X : "); Serial.print(ax);
+  Serial.print("g / Acceleration_Y : "); Serial.print(ay);
+  Serial.print("g / Acceleration_Z : "); Serial.print(az);
   Serial.print("g / LUX : ");
   if (lux.light) {
     Serial.print(lux.light); Serial.print("lux");
   } else {
     Serial.print("'Sensor overload'");
   }
-  Serial.print(" / microphone : "); Serial.print(microphone);
-  Serial.print(" / parallax : "); Serial.print(pir); Serial.println("");
-
-  delay(1000);
+  Serial.print(" / mic : "); Serial.print(microphone);
+  Serial.print(" / parallax : "); Serial.println(ans);
 }
