99cafab02f52c32bf3618ba37b95f03a08a92799
shoichi yokobori <shoichi@MacBook-Pro-shoichi.local>
2016-12-08 01:35:50 +0900

    can experiment

--- class_graduation_work.ino
+++ class_graduation_work.ino
@@ -15,15 +15,24 @@ double temp_act, press_act, hum_act;   // 温湿度気圧センサ変数宣言
 MPU6050 accelgyro;   // 6軸加速度センサ
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
+const float factor = 0.99;
+float ax_low_filter_old = 0.0;
+float ay_low_filter_old = 0.0;
+float az_low_filter_old = 0.0;
 
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);   // 照度センサ
 sensors_event_t lux;
 
-int mic_pin = 3;   // 音センサのピン
-int microphone;
+const int mic_analogpin = 3;   // 音センサのピン
+int sound;
 
-int pir_pin = 13;   // 焦電センサのピン
+const int pir_pin = 13;   // 焦電センサのピン
 int ans;
+
+
+double old_temp_act, old_press_act, old_hum_act;
+float old_acceleration, old_lux;
+int old_sound;
 
 void setup() {
   uint8_t osrs_t = 1;   // Temperature oversampling x 1
@@ -43,40 +52,92 @@ void setup() {
   accelgyro.initialize();
 
 // ------------------------------- 現在の時刻を入れて実験開始 -------------------------------
-  setTime(12, 38, 0, 25, 10, 2016);
+  setTime(12, 38, 0, 8, 12, 2016);
 }
 
 void loop() {
-  bme280.readData(&temp_act, &press_act, &hum_act);   // 温湿度気圧センサの値を読み込む
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);   // 6軸加速度センサの値を読み込む
-  tsl.getEvent(&lux);   // 照度センサの値を読み込む
-  microphone = analogRead(mic_pin);   // 音センサの値を読み込む
-  microphone = map(microphone, 0, 1023, -511, 512);
   ans = digitalRead(pir_pin);   // 赤外線センサの値を読み込む
-  // String pir = ans==0 ? "LOW" : "HIGH";
+  sensing();
+  float acceleration = 0.0;
+  acceleration = total_acceleration();
 
   if(ans == 1){
-    sensing_output();
+    sensing_start_print();   // 時刻の表示
+    sensing_print(old_temp_act, old_press_act, old_hum_act, old_acceleration, old_lux, old_sound);
+    for(int i=0; i < 1000; i++){
+      sensing_print(temp_act, press_act, hum_act, acceleration, lux.light, sound);
+      delay(300);
+      sensing();
+      acceleration = total_acceleration();
+      if(i > 3 && digitalRead(pir_pin) == 0){
+        break;
+      }
+    }
+    Serial.println("");
   }
-  delay(100);
+
+  old_temp_act = temp_act;
+  old_press_act = press_act;
+  old_hum_act = hum_act;
+  old_acceleration = acceleration;
+  old_lux = lux.light;
+  old_sound = sound;
+  delay(500);
 }
 
 
-void sensing_output() {
-  Serial.print(year()); Serial.print("/"); Serial.print(month()); Serial.print("/"); Serial.print(day()); Serial.print(" ");
+
+float total_acceleration() {
+  float total_acceleration = 0.0;
+  for(int i=0; i<20; i++){
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    float ax_low_filter = factor * ax_low_filter_old + (1-factor) * (float)ax;
+    float ax_high_filter = factor * (ax_high_filter + ax_low_filter - ax_low_filter_old);
+    float ay_low_filter = factor * ay_low_filter_old + (1-factor) * (float)ay;
+    float ay_high_filter = factor * (ay_high_filter + ay_low_filter - ay_low_filter_old);
+    float az_low_filter = factor * az_low_filter_old + (1-factor) * (float)az;
+    float az_high_filter = factor * (az_high_filter + az_low_filter - az_low_filter_old);
+    total_acceleration += (ax_high_filter * ax_high_filter + ay_high_filter * ay_high_filter + az_high_filter * az_high_filter)/20;
+    ax_low_filter_old = ax_low_filter;
+    ay_low_filter_old = ay_low_filter;
+    az_low_filter_old = az_low_filter;
+  }
+  return total_acceleration;
+}
+
+
+/* センシングの出力結果
+
+   12/8 5:30:20秒の場合
+>
+> ------------------  12/8 5:30:20  ------------------
+>
+
+*/
+void sensing_start_print() {
+  Serial.print("------------------  ");
+  Serial.print(month()); Serial.print("/"); Serial.print(day()); Serial.print(" ");
   Serial.print(hour()); Serial.print(":"); Serial.print(minute()); Serial.print(":"); Serial.print(second());
-  Serial.print("  Temperature : "); Serial.print(temp_act);
+  Serial.println("  ------------------");
+}
+
+void sensing() {
+  bme280.readData(&temp_act, &press_act, &hum_act);   // 温湿度気圧センサの値を読み込む
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);   // 6軸加速度センサの値を読み込む
+  tsl.getEvent(&lux);   // 照度センサの値を読み込む
+  sound = analogRead(mic_analogpin);   // 音センサの値を読み込む
+}
+
+void sensing_print(double temp_act, double press_act, double hum_act, float acceleration, float lux, int sound) {
+  Serial.print("Temperature : "); Serial.print(temp_act);
   Serial.print("DegC / Pressure : "); Serial.print(press_act);
   Serial.print("hPa / Humidity : "); Serial.print(hum_act);
-  Serial.print("% / Acceleration_X : "); Serial.print(ax);
-  Serial.print("g / Acceleration_Y : "); Serial.print(ay);
-  Serial.print("g / Acceleration_Z : "); Serial.print(az);
+  Serial.print("% / Acceleration : "); Serial.print(acceleration);
   Serial.print("g / LUX : ");
-  if (lux.light) {
-    Serial.print(lux.light); Serial.print("lux");
+  if (lux) {
+    Serial.print(lux); Serial.print("lux");
   } else {
     Serial.print("'Sensor overload'");
   }
-  Serial.print(" / mic : "); Serial.print(microphone);
-  Serial.print(" / parallax : "); Serial.println(ans);
+  Serial.print(" / sound : "); Serial.println(sound);
 }
